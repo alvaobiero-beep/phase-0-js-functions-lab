6979e4f490399a8a04b75cec4c6860cd10522ce2
Alva <alvaobiero@gmail.com>
2026-09-25 08:23:47 +0300

    javascript lab

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
+function calculateTax(amount) {
+  console.log (amount * 0.10)
+}
 
+function convertToUpperCase(text) {
+console.log (text.toUpperCase())
+}
+
+function findMaximum(num1, num2) {
+  console.log ( Math.max(num1, num2))
+}
+
+function isPalindrome(word) {
+  const reversed = word.split('').reverse().join('');
+  console.log (word === reversed)
+}
+
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+  console.log( originalPrice * (1 - discountPercentage / 100))
+}
 
 
 
